bed7ab8a26931f1f7f6edd4c778af8b09d64004d
gal kahana <gal.kahana@hotmail.com>
2020-10-09 12:19:48 +0300

    samples corrections and updates

--- examples/simple-script/index.js
+++ examples/simple-script/index.js
@@ -7,7 +7,7 @@ const { PDFEngine } = require('../../')
 const ASSETS_FOLDER = './assets'
 
 async function createPDFFile() {
-    const jobTicket = require('./assets/documentForReport.json')
+    const theDocument = require('./assets/documentForReport.json')
     const assetsMap = {
         '!': path.resolve(__dirname, ASSETS_FOLDER, '!.pdf'),
         'pngLogo': path.resolve(__dirname, ASSETS_FOLDER, 'original.png'),
@@ -18,7 +18,7 @@ async function createPDFFile() {
 
     const engine = new PDFEngine(assetsMap)
     await mkdirp(path.resolve(__dirname,'./output/'))
-    engine.generatePDF(jobTicket.document.embedded, new PDFWStreamForFile(path.resolve(__dirname,'./output/result.pdf')))
+    engine.generatePDF(theDocument, new PDFWStreamForFile(path.resolve(__dirname,'./output/result.pdf')))
 }
 
 
